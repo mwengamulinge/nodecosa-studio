181e81e94c968b91874330627e01eeefb638d08a
Ram Mulinge <87355298+mwengamulinge@users.noreply.github.com>
2025-12-22 23:38:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    { enforce: 'pre', ...mdx() },
+    react(),
+  ],
   server: {
     port: 3000,
   },
